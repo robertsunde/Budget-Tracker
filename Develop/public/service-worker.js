acf224b117c8a67f377aa10f911f1da26292bb5c
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-04-13 19:05:06 -0400

    creating service worker

---
+++ Develop/public/service-worker.js
@@ -0,0 +1,9 @@
+const cacheData = [
+"/",
+"/Budget-Tracker/Develop/public/db.js",
+"/Budget-Tracker/Develop/public/index.html",
+"/Budget-Tracker/Develop/public/index.js",
+"/Budget-Tracker/Develop/public/service-worker.js",
+"/Budget-Tracker/Develop/public/styles.css"
+
+]
