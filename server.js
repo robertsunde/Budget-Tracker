b7ca0be157757114dea73f02c36e7bdefb314144
Robert Sunde <73792987+robertsunde@users.noreply.github.com>
2021-04-14 20:41:07 -0400

    testing heroku 999th time

--- server.js
+++ server.js
@@ -37,9 +37,8 @@ mongoose.connect(
 
 // routes
 app.use(require("./routes/api.js"));
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`App running on port ${PORT}`);
 });
 
 //
